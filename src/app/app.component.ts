3284c81efc01470a7dc236e02488160a1ce3b18e
SergioBJ <Serbeju00@gmail.com>
2024-07-17 20:08:48 -0500

    firma3.0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { LayoutComponent } from './containers/layout/layout.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, LayoutComponent],
+  imports: [CommonModule, RouterOutlet, LayoutComponent, RouterLink],
   template: '<app-layout/>',
 })
 export class AppComponent {
--- src/app/containers/navbar/navbar.component.ts
+++ src/app/containers/navbar/navbar.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
+import { RouterOutlet } from '@angular/router';
 // In your component.ts file
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [MatIconModule],
+  imports: [MatIconModule, RouterOutlet],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss',
 })
