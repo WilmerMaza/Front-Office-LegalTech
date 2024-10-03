6b7e82099708d20a77834defae93cbe04837350a
Ale13258 <aleja13258@gmail.com>
2024-10-03 00:02:23 -0500

    correciones

--- src/app/views/team/team.component.ts
+++ src/app/views/team/team.component.ts
@@ -1,10 +1,6 @@
 import { Component } from '@angular/core';
 import { ButtonTeamComponent } from './components/button-team/button-team.component';
-import { ExperienceTeamComponent } from './components/experience-team/experience-team.component';
 import { ImgTeamComponent } from './components/img-team/img-team.component';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatSelectModule } from '@angular/material/select';
-import { MatInputModule } from '@angular/material/input';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
@@ -13,11 +9,7 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   imports: [
     ImgTeamComponent,
-    ExperienceTeamComponent,
     ButtonTeamComponent,
-    MatFormFieldModule,
-    MatSelectModule,
-    MatInputModule,
     FormsModule,
     CommonModule
   ],
@@ -25,28 +17,28 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./team.component.scss']
 })
 export class TeamComponent {
-  showInfo: boolean = false;
-  showEducation: boolean = false;
-  showSignature: boolean = false;
 
+  public showInfo: boolean = false;
+  public showEducation: boolean = false;
+  public showSignature: boolean = false;
 
-  arrowContact: string = 'assets/img/team/arriba.png';
-  arrowFormation: string = 'assets/img/team/arriba.png';
+  get arrowContact(): string {
+    return this.showInfo ? 'assets/img/team/abajo.png' : 'assets/img/team/arriba.png';
+  }
 
-  toggleInfo() {
+  get arrowFormation(): string {
+    return this.showEducation ? 'assets/img/team/abajo.png' : 'assets/img/team/arriba.png';
+  }
+
+  public toggleInfo(): void {
     this.showInfo = !this.showInfo;
-
-    this.arrowContact = this.showInfo ? 'assets/img/team/abajo.png' : 'assets/img/team/arriba.png';
   }
 
-  toggleEducation() {
+  public toggleEducation(): void {
     this.showEducation = !this.showEducation;
-
-    this.arrowFormation = this.showEducation ? 'assets/img/team/abajo.png' : 'assets/img/team/arriba.png';
   }
 
-  toggleSignature() {
+  public toggleSignature(): void {
     this.showSignature = !this.showSignature;
   }
 }
-
