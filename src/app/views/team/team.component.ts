17acc21afacf072c6916ec72036d2f61104071db
Ale13258 <aleja13258@gmail.com>
2024-10-08 23:12:46 -0500

    nuevos cambios

--- src/app/views/team/team.component.ts
+++ src/app/views/team/team.component.ts
@@ -3,6 +3,7 @@ import { ButtonTeamComponent } from './components/button-team/button-team.compon
 import { ImgTeamComponent } from './components/img-team/img-team.component';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-team',
@@ -25,6 +26,7 @@ export class TeamComponent {
   public arrowContact: string = 'assets/img/team/arriba.png';
   public arrowFormation: string = 'assets/img/team/arriba.png';
 
+
   public toggleInfo(): void {
     this.showInfo = !this.showInfo;
     this.arrowContact = this.showInfo ? 'assets/img/team/abajo.png' : 'assets/img/team/arriba.png';
@@ -37,6 +39,12 @@ export class TeamComponent {
 
   public toggleSignature(): void {
     this.showSignature = !this.showSignature;
-  }
-}
 
+  }
+  htmlString: string = '<p> Abogada con amplia experiencia en <strong>asesoría legal y litigios</strong>, con más de <strong>21 años de experiencia</strong> en el ejercicio del derecho. En materia de derecho comercial y civil, ha asesorado empresas como Boccard Colombia SAS, Tenaris-Tubo Caribe Ltda, Hotel Casa del Coliseo, Hotel Bahía S.A.S, Vélez Gutiérrez Abogados S.A.S, Russell Bedford DSA, Igualmente, atención de procesos laborales y administrativos como abogada  corresponsal de la firma Chapman & Asociados, Vélez Gutiérrez, Baker & McKenzie S.A.S, atendiendo empresas como Royal & Sun Alliance, Ecopetrol, Coca Cola, Liberty SA, BBVA Colombia SA, ARL SURA, Corpbanca, Bancolombia, Círculo de Lectores, entre otros. </p>';
+  safeHtml: SafeHtml;
+
+  constructor(private sanitizer: DomSanitizer) {
+    this.safeHtml = this.sanitizer.bypassSecurityTrustHtml(this.htmlString);
+}
+}
