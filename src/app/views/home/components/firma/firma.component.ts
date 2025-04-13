8759a675be9bf49b38a29013e731a2537af2b180
carlos <carlosandresar32@gmail.com>
2025-04-13 13:55:15 -0500

    traductor de ingles a español carlos arteaga

--- src/app/views/home/components/firma/firma.component.ts
+++ src/app/views/home/components/firma/firma.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { TranslateModule } from '@ngx-translate/core';
 
+
+
 @Component({
   selector: 'app-firma',
   standalone: true,
