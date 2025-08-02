b8cbfb44612ef785bb8d3acc8428bf535e196a57
WilmerMaza <wilmermaza10@gmail.com>
2025-08-02 12:53:14 -0500

    occultar personal

--- src/app/constant/team.ts
+++ src/app/constant/team.ts
@@ -38,81 +38,81 @@ export const TEAMARRAY: TeamArray[] = [
        `,
     description: '',
   },
-  {
-    person: 'img/team/MIrna.png',
-    img: 'img/team/member-mirna.png',
-    img_movil: 'img/team/member-mirna-movil.png',
-    name: 'MIRNA AGUILERA',
-    link: 'MIRNA_AGUILERA',
+  // {
+  //   person: 'img/team/MIrna.png',
+  //   img: 'img/team/member-mirna.png',
+  //   img_movil: 'img/team/member-mirna-movil.png',
+  //   name: 'MIRNA AGUILERA',
+  //   link: 'MIRNA_AGUILERA',
 
-    position: 'TEAM.MIRNA.POSITION',
-    educacion: 'TEAM.MIRNA.EDUCATION',
-    formacion: 'TEAM.MIRNA.FORMACION',
-    contacto: `
-       <table>
-    <tbody>
-      <tr>
-        <th scope="row">
-          <img src="icons/telefono.svg" alt="Teléfono" loading="lazy" width="24" height="24" />
-        </th>
-        <td>
+  //   position: 'TEAM.MIRNA.POSITION',
+  //   educacion: 'TEAM.MIRNA.EDUCATION',
+  //   formacion: 'TEAM.MIRNA.FORMACION',
+  //   contacto: `
+  //      <table>
+  //   <tbody>
+  //     <tr>
+  //       <th scope="row">
+  //         <img src="icons/telefono.svg" alt="Teléfono" loading="lazy" width="24" height="24" />
+  //       </th>
+  //       <td>
 
-          <a href="tel:3053072539"> 305 3072539</a>
+  //         <a href="tel:3053072539"> 305 3072539</a>
 
 
-        </td>
-      </tr>
-      <tr>
-        <th scope="row">
-          <img src="icons/email.svg" alt="Correo electrónico" loading="lazy" width="24" height="24" />
-        </th>
-        <td>
-          <a href="mailto:mirnaaguilera&#64;abogadosdigitale.com.co" >
-            mirnaaguilera&#64;abogadosdigitale.com.co
-          </a>
-        </td>
-      </tr>
-    </tbody>
-  </table>
-      `,
-    experiencia: 'TEAM.MIRNA.EXPERIENCE',
-    description: '',
-  },
-  {
-    person: 'img/team/isabel_pinto.png',
-    img: 'img/team/member-isabel.png',
-    img_movil: 'img/team/member-isabel-movil.png',
-    name: 'ISABEL PINTO',
-    link: 'ISABEL_PINTO',
-    position: 'TEAM.ISABEL.POSITION',
-    educacion: 'TEAM.ISABEL.EDUCATION',
-    formacion: 'TEAM.ISABEL.FORMACION',
-    contacto: `
-        <table>
-    <tbody>
-      <tr>
-        <th scope="row">
-          <img src="icons/telefono.svg" alt="Teléfono" loading="lazy" width="24" height="24" />
-        </th>
-        <td>
-          <a href="tel:3234339675">323 4339675</a>
+  //       </td>
+  //     </tr>
+  //     <tr>
+  //       <th scope="row">
+  //         <img src="icons/email.svg" alt="Correo electrónico" loading="lazy" width="24" height="24" />
+  //       </th>
+  //       <td>
+  //         <a href="mailto:mirnaaguilera&#64;abogadosdigitale.com.co" >
+  //           mirnaaguilera&#64;abogadosdigitale.com.co
+  //         </a>
+  //       </td>
+  //     </tr>
+  //   </tbody>
+  // </table>
+  //     `,
+  //   experiencia: 'TEAM.MIRNA.EXPERIENCE',
+  //   description: '',
+  // },
+  // {
+  //   person: 'img/team/isabel_pinto.png',
+  //   img: 'img/team/member-isabel.png',
+  //   img_movil: 'img/team/member-isabel-movil.png',
+  //   name: 'ISABEL PINTO',
+  //   link: 'ISABEL_PINTO',
+  //   position: 'TEAM.ISABEL.POSITION',
+  //   educacion: 'TEAM.ISABEL.EDUCATION',
+  //   formacion: 'TEAM.ISABEL.FORMACION',
+  //   contacto: `
+  //       <table>
+  //   <tbody>
+  //     <tr>
+  //       <th scope="row">
+  //         <img src="icons/telefono.svg" alt="Teléfono" loading="lazy" width="24" height="24" />
+  //       </th>
+  //       <td>
+  //         <a href="tel:3234339675">323 4339675</a>
 
-        </td>
-      </tr>
-      <tr>
-        <th scope="row">
-          <img src="icons/email.svg" alt="Correo electrónico" loading="lazy" width="24" height="24" />
-        </th>
-        <td>
-          <a href="mailto:Isabelpinto&#64;abogadosdigitales.com.co" >
-            Isabelpinto&#64;abogadosdigitales.com.co
-          </a>
-        </td>
-      </tr>
-    </tbody>
-  </table>
-      `,
-    experiencia: 'TEAM.ISABEL.EXPERIENCE',
-    description: 'TEAM.ISABEL.DESCRIPTION',
-  },
+  //       </td>
+  //     </tr>
+  //     <tr>
+  //       <th scope="row">
+  //         <img src="icons/email.svg" alt="Correo electrónico" loading="lazy" width="24" height="24" />
+  //       </th>
+  //       <td>
+  //         <a href="mailto:Isabelpinto&#64;abogadosdigitales.com.co" >
+  //           Isabelpinto&#64;abogadosdigitales.com.co
+  //         </a>
+  //       </td>
+  //     </tr>
+  //   </tbody>
+  // </table>
+  //     `,
+  //   experiencia: 'TEAM.ISABEL.EXPERIENCE',
+  //   description: 'TEAM.ISABEL.DESCRIPTION',
+  // },
 ];
